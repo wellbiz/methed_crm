45df183b2b1a55feec78bd6db8790c0725bd906e
Dmitry Taras <wellbiz@ya.ru>
2022-01-02 21:12:46 +0300

    added lesson 2 module 2

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
-const sum = (a, b) => a + b;
-
-sum(3, 3);
+const modalTitle = document.querySelector('.modal__title');
+const modalForm = document.querySelector('.modal__form');
+const modalCheckbox = document.querySelector('.modal__checkbox');
+const modalDiscount = document.querySelector('.modal__input_discount');
