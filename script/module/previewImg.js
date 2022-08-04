7a6af2c1861d1d35d649202d693e3154c5da4a57
My Name <my-name@chromium.org>
2022-08-04 22:21:07 +0300

    исправил: алерт в своем поле.

--- script/module/previewImg.js
+++ script/module/previewImg.js
@@ -1,14 +1,4 @@
-// const file = document.querySelector(".file");
-// const preview = document.querySelector(".preview");
-// const form = document.querySelector(".form-input");
 
-// file.addEventListener("change", () => {
-//   if (file.files.length > 0) {
-//     const src = URL.createObjectURL(file.files[0]);
-//     preview.src = src;
-//     preview.style.display = "block";
-//   }
-// });
 
 export const createPreview = () => {
   const img = document.createElement("img");
@@ -19,7 +9,7 @@ export const createPreview = () => {
 export const previewImg = () => {
   const preview = document.querySelector(".preview");
   const file = document.querySelector(".modal__file");
-  const alertText = document.querySelector(".modal__total");
+  const alertText = document.querySelector(".modal_alert_text");
   file.addEventListener("change", () => {
     const MAX_SIZE_IMG = 1024 * 1024;
     if (file.files.length > 0 && file.files[0].size < MAX_SIZE_IMG) {
@@ -28,8 +18,7 @@ export const previewImg = () => {
       preview.style.display = "block";
       preview.style.paddingBottom = "10px";
 
-      alertText.style.color = "#6E6893";
-      alertText.textContent = "Итоговая стоимость:";
+      alertText.textContent = "";
     } else {
       preview.style.display = "none";
       alertText.style.color = "red";
